ac78269a6d586776ccf52e6ead9806344a1f1fd3
tommyboy51o <72840477+tommyboy51o@users.noreply.github.com>
2020-10-15 18:36:39 -0700

    Update Hello.java

--- Assignment 1/practice/Hello.java
+++ Assignment 1/practice/Hello.java
@@ -3,7 +3,8 @@ import java.io.*;
 class Hello{
 	
 	public void printMessage(){
-		System.out.println("First Message --REMOVE");
+		System.out.println("First Message");
+		System.out.println("Second Message --REMOVE");
 		
 	}
 	
