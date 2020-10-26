8b7eb57879ffffcd2ba72e5438104c446a858cf9
tommyboy51o <72840477+tommyboy51o@users.noreply.github.com>
2020-10-26 15:42:47 -0700

    For testing random number generator

---
+++ Assignment 2/runtest files/Randomizer.java
@@ -0,0 +1,8 @@
+public class Randomizer {
+    public static void main(String[] args) {
+        System.out.println(Math.random() + " tries");
+        System.out.println("this was done " + Math.random() + " tries");
+        System.out.println("The number of times this was tried is " + Math.random());
+    }
+}
+
