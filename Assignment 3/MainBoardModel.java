6617dd6ec0b0d7b4de52508037f23baa4019954c
tommyboy51o <72840477+tommyboy51o@users.noreply.github.com>
2020-11-06 06:30:49 -0800

    Create MainBoardModel.java

---
+++ Assignment 3/MainBoardModel.java
@@ -0,0 +1,7 @@
+public class MainBoard {
+	private CardRowModel cardRow;
+	private IslandModel island;
+	private ResearchTrackModel researchTrack;
+	private SupplyBoardModel supplyboard;
+	private ArrayList<Player> players;
+}
