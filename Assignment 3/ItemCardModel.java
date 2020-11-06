9f5f1147c57c1ddee1a6d8152d24daa1ce100e6f
Jaegook Lee <jlee205@horizon.csueastbay.edu>
2020-11-05 18:38:56 -0800

    added to ItemCardModel

--- Assignment 3/ItemCardModel.java
+++ Assignment 3/ItemCardModel.java
@@ -1,4 +1,17 @@
+import java.util.ArrayList;
 
-public class ItemCardModel {
-
+public class ItemCardModel extends CardModel {
+	int costToBuy;
+	
+	public ItemCardModel(String travelValue, int travelPoints, ArrayList<String> effects, boolean isActionFree, int points, int costToBuy) {
+		
+		this.travelValue = travelValue;
+		this.travelPoints = travelPoints;
+		this.effects = effects;
+		this.isActionFree = isActionFree;
+		this.points = points;
+		this.costToBuy = costToBuy;
+	}
+	public int getCostToBuy() { return costToBuy; }
+	public void setCostToBuy(int costToBuy) { this.costToBuy = costToBuy; }
 }
