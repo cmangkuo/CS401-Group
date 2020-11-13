296ea34de6656ae60e288cc997b47e52e368d8c5
The-Big-Potato <45223423+The-Big-Potato@users.noreply.github.com>
2020-11-13 15:39:53 -0800

    Revert "Updated CardView"
    This reverts commit bf43d2b44b9cf594a51cd12de42b0db739b4fbb0.

--- Assignment 3/ArtifactCardModel.java
+++ Assignment 3/ArtifactCardModel.java
@@ -1,3 +1,4 @@
+
 public class ArtifactCardModel extends CardModel{
 		int costToBuy;
 		int costToPlay;
--- Assignment 3/CardView.java
+++ Assignment 3/CardView.java
@@ -1,14 +1,5 @@
-import java.util.ArrayList;
-
 public class CardView {
     public void printCardDetails(CardModel model){
-        System.out.println("Card: ");
-        System.out.println("Effect: " + model.effect);
-        System.out.println("Travel Value: " + model.travelValue);
-        System.out.println("Travel points: " + model.travelPoints);
-        System.out.println("IsFreeAction: " + model.isFreeAction);
-        System.out.println("Points: " + model.points);
-        System.out.println("IsActionFree: " + model.isActionFree);
+
     }
 }
-
