e349f0c016cc3dad50f8881a2388a52ab8f5aac7
The-Big-Potato <45223423+The-Big-Potato@users.noreply.github.com>
2020-11-13 15:39:48 -0800

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit f8602f6a051caa88e04ebc9a1ff50b022f56330b, reversing
    changes made to bf43d2b44b9cf594a51cd12de42b0db739b4fbb0.

--- Assignment 3/CardModel.java
+++ Assignment 3/CardModel.java
@@ -1,16 +1,19 @@
 import java.util.ArrayList;
+
 public class CardModel {
 	String travelValue;		//the type of travel: foot, plane, car, or boat
 	int travelPoints;		//number of travel points on the card (# of travel points = # of travelValue icons on the card) 
-	ArrayList<String> effects;
-	boolean isActionFree; // true means it doesn't count as your main action, false means it does count as your main action
+	String effect;		  
+	boolean isFreeAction; // true means it doesn't count as your main action, false means it does count as your main action
 	int points;
+	ArrayList<String> effects;
+	boolean isActionFree;
 	
 	public CardModel() {}
 	
 	public String getTravelValue() { return travelValue; }
-	public ArrayList<String> getEffects() { return effects; }
+	public String getEffect() { return effect; }
 	public int getPoints() { return points; }
 	public int getTravelPoints() { return travelPoints; }
-	public boolean isActionFree() { return isActionFree; }
+	public boolean isFreeAction() { return isFreeAction; }
 }
