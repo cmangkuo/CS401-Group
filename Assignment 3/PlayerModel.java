91540a35e1b812585008b770557c12ee26ae294a
Jaegook Lee <jlee205@horizon.csueastbay.edu>
2020-11-13 13:28:48 -0800

    expanded PlayerModel and PlayerBoardModel

--- Assignment 3/PlayerModel.java
+++ Assignment 3/PlayerModel.java
@@ -1,33 +1,75 @@
 import java.util.ArrayList;
-
-/*Each player has a player board, player number, play area and research tokens.
-there are more things inside each player board
-each player start with 4 basic cards which is different from item/fear/artifact
-each player also get 2 fear cards*/
 public class PlayerModel {
-  private int id; //player number
-	private ArrayList<CardModel> playArea; //like a graveyard for used cards and is sent to bottom of deck after round ends
-	private PlayerBoardModel playerBoard; //player board
-	private int researchTokens; //number of research tokens
-
-	//methods:
-	//every player draws at the beginning of their turn to have 5 cards always
-	public void Draw(){}
-
-	/*every player can do 1 main action and as many free action
-	main actions are: dig at a site, discover a new site, overcome a guardian,
-	buy a card, play a card, research, or pass.
-	any card with a lightning bolt is a free action */
-	public void Dig(){}
-	public void Discover(){}
-	public void OvercomeGuardian(){}
-	public void Buy(){}
-	public void Play(){}
-	public void Research(){}
+	String name; //Name of player (player1, player2, player3, or player4)
+	PlayerBoardModel playerBoard;
+	boolean mainActionDone = false; // true if player has performed a main action.
+	public PlayerModel(String name) {
+		this.name = name;
+		playerBoard = new PlayerBoardModel();
+	}
 	
-	//indicate that turn is over.
-	public void Pass(){}
-
-	//all cards played are shuffled and returned to the bottom of your own deck
-	public void SetUpNextRound(){}
+	public PlayerBoardModel getPlayerBoard() {
+		return playerBoard;
+	}
+	public void draw() {
+		playerBoard.draw();
+	}
+	public void collectResources(String resource, int amount) {
+		playerBoard.gainResource(resource, amount);
+	}
+	public void payResources(String resource, int amount) {
+		playerBoard.payResource(resource, amount);
+	}
+	
+	public void setMainActionDone(boolean mainAct) {
+		mainActionDone = mainAct;
+	}
+	public void freeAction(CardModel card) {
+			if (card instanceof FundingCardModel) {
+				//This is a FundingCard so you gain one coin
+				collectResources("coin", 1);
+			}
+			if (card instanceof ExplorationCardModel) {
+				//this is an ExplorationCard, gain one compass
+				collectResources("compass", 1);
+			}
+	}
+	
+	
+	//MAIN ACTIONS: Dig at a site, discover a new site, overcome a guardian, buy a card, play a card, research , and pass
+	
+	public void dig() {
+		mainActionDone = true;
+	}
+	public void discover() {
+		mainActionDone = true;
+	}
+	public void overcome() {
+		mainActionDone = true;
+	}
+	public void buyCard() {
+		mainActionDone = true;
+	}
+	
+	public void playCard(CardModel card) {
+		if (card.isActionFree()) {
+			freeAction(card);
+		}else {
+			mainActionDone = true;
+			ArrayList<String>effects = card.getEffects();
+			for (String effect : effects) {
+				// resolve the effect.
+			}
+			//Put card into playarea
+			playerBoard.play(card);
+			
+		}
+	}
+	public void research() {
+		mainActionDone = true;
+	}
+	public void pass() {
+		mainActionDone = true;
+	}
 }
+
