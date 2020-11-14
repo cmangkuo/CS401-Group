bb4d9906e65d473a2d9cee6d5c8cec62e0179581
The-Big-Potato <45223423+The-Big-Potato@users.noreply.github.com>
2020-11-13 17:31:26 -0800

    Update CardView.java

--- Assignment 3/CardView.java
+++ Assignment 3/CardView.java
@@ -1,5 +1,10 @@
 public class CardView {
     public void printCardDetails(CardModel model){
+		System.out.println("Card: ");
+        System.out.println("Travel Value: " + model.travelValue);
+        System.out.println("Travel points: " + model.travelPoints);
+        System.out.println("Points: " + model.points);
+        System.out.println("IsActionFree: " + model.isActionFree);
 
     }
 }
