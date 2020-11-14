693264efa8c70c42036da1f2bb9b294ce3d185da
tommyboy51o <72840477+tommyboy51o@users.noreply.github.com>
2020-11-13 18:24:21 -0800

    Update ItemCardView.java

--- Assignment 3/ItemCardView.java
+++ Assignment 3/ItemCardView.java
@@ -2,9 +2,9 @@ public class ItemCardView {
   public void printItemCardStats(ItemCardModel model) {
     System.out.println("Travel value: " + model.travelValue+
                        "\nTravel points: " + model.travelPoints+
-                       "\nEffects: " + model.travelPoints+
-                       "\nFree Action: " + model.travelPoints+
-                       "\nPoints: " + model.travelPoints+
-                       "\nCost to Buy: " + model.travelPoints");
+                       "\nEffects: " + model.effects+
+                       "\nFree Action: " + model.isActionFree+
+                       "\nPoints: " + model.points+
+                       "\nCost to Buy: " + model.costToBuy");
   }
 }
