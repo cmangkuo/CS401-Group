3cc10409a5046cf5f30cba2bda43a609b8205b98
tommyboy51o <72840477+tommyboy51o@users.noreply.github.com>
2020-11-20 21:39:18 -0800

    Update ArchaeologistModel.java

--- Assignment 3/ArchaeologistModel.java
+++ Assignment 3/ArchaeologistModel.java
@@ -1,5 +1,26 @@
 
 
 public class ArchaeologistModel {
-	public ArchaeologistModel() {}
+	private SiteModel site;
+	private boolean free;
+	
+	public ArchaeologistModel() {
+		free = true;
+	}
+	
+	public SiteModel getSite() {
+		return site;
+	}
+	
+	public void setSite(SiteModel model) {
+		site = model;
+	}
+	
+	public boolean getFree() {
+		return free;
+	}
+	
+	public boolean setFree(boolean freedom) {
+		free = freedom;
+	}
 }
