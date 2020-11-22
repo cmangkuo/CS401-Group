b60f96abdd45dc868651407d851b21c41bc81778
The-Big-Potato <45223423+The-Big-Potato@users.noreply.github.com>
2020-11-21 23:46:40 -0800

    Update FearCardView

--- Assignment 3/FearCardView.java
+++ Assignment 3/FearCardView.java
@@ -1,2 +1,10 @@
 public class FearCardView {
+    public void printFearCard(FearCardModel model){
+        System.out.println("Player: ");
+        System.out.println("Name: " + model.travelValue);
+        System.out.println("Travel points: " + model.travelPoints);
+        System.out.println("Points: " + model.points);
+
+
+    }
 }
