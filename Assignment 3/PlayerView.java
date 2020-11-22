7a5a0f4c3d081d08308f353e2145f84248993556
The-Big-Potato <45223423+The-Big-Potato@users.noreply.github.com>
2020-11-21 23:42:45 -0800

    Update PlayerView

--- Assignment 3/PlayerView.java
+++ Assignment 3/PlayerView.java
@@ -1,2 +1,9 @@
 public class PlayerView {
+    public void printPlayerDetails(PlayerModel model){
+        System.out.println("Player: ");
+        System.out.println("Name: " + model.name);
+        System.out.println("Main action done: " + model.mainActionDone);
+
+
+    }
 }
