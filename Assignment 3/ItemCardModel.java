ebdaff6de0ec4c17c0d9b0ab8e7a28db9c99055c
tommyboy51o <72840477+tommyboy51o@users.noreply.github.com>
2020-12-03 20:02:07 -0800

    Update ItemCardModel.java

--- Assignment 3/ItemCardModel.java
+++ Assignment 3/ItemCardModel.java
@@ -3,11 +3,12 @@ import java.util.ArrayList;
 public class ItemCardModel extends CardModel {
 	int costToBuy;
 	
-	public ItemCardModel(String travelValue, int travelPoints, String effects, boolean isActionFree, int costToBuy) {
+	public ItemCardModel(String travelValue, int travelPoints, String effects, int points, boolean isActionFree, int costToBuy) {
 		
 		this.travelValue = travelValue;
 		this.travelPoints = travelPoints;
 		this.effects = effects;
+		this.points = points;
 		this.isActionFree = isActionFree;
 		this.costToBuy = costToBuy;
 	}
