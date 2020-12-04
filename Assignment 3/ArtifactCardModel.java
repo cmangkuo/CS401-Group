fb549de646cccb540f2a676a4965fd24dacb7960
tommyboy51o <72840477+tommyboy51o@users.noreply.github.com>
2020-12-03 19:57:28 -0800

    Update ArtifactCardModel.java

--- Assignment 3/ArtifactCardModel.java
+++ Assignment 3/ArtifactCardModel.java
@@ -3,11 +3,10 @@ public class ArtifactCardModel extends CardModel{
 		int costToBuy;
 		int costToPlay;
 		boolean isBought = false;
-	public ArtifactCardModel (String travelValue, String effect, int points, int costToBuy, int costToPlay, int travelPoints) {
+	public ArtifactCardModel (String travelValue, String effect, int costToBuy, int costToPlay, int travelPoints) {
 		this.isFreeAction = false;	
 		this.travelValue = travelValue;
 		this.effect = effect;
-		this.points = points;
 		this.costToBuy = costToBuy;
 		this.costToPlay = costToPlay;
 		this.travelPoints = travelPoints;
