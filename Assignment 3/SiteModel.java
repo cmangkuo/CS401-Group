ea05352aa642d8248ae74514806f4cc59fce1912
tommyboy51o <72840477+tommyboy51o@users.noreply.github.com>
2020-12-05 11:52:27 -0800

    Update SiteModel.java

--- Assignment 3/SiteModel.java
+++ Assignment 3/SiteModel.java
@@ -2,15 +2,13 @@ public class SiteModel {
         private boolean room;
         private int max;
         private int space;
-        private String resourceName;
         private int resourceAmount;
         private int travelCost;
         private int tile; //0 = free, 1 = line tile (pay 3 exploration coins), 2 = double line tile (pay 6 exploration coins)
         
-        public SiteModel(String resource, int resAmt, int t){
+        public SiteModel(int resAmt, int t){
                 max = (Math.random() <= 0.5) ? 1 : 2;
                 travelCost = (Math.random()*8)+1; //travel cost, 1 = foot,  2 = car, 3 = boat, 4 = plane, 5-8 is double of each ex: 5 is 2 foot icons.
-                resourceName = resource;
                 resourceAmount = resAmt;
                 space = 0;
                 tiles = t;
